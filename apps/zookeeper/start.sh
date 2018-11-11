8021dfedae225af60439653900753dc408271ff3
lxcyha <253314157@qq.com>
2018-11-11 11:00:19 +0800

    zookeeper

--- apps/zookeeper/start.sh
+++ apps/zookeeper/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker stop zookeeper
 docker rm zookeeper
-docker run --name zookeeper -p 2181:2181 --restart always -d zookeeper:lastest
+docker run --name zookeeper -p 2181:2181 --restart always -d zookeeper:latest
