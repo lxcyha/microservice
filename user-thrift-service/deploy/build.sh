e5fdf28aa6e6dbc784d974b2682e7edbd4a80069
lxcyha <253314157@qq.com>
2018-11-27 01:11:44 +0800

    docker

--- user-thrift-service/deploy/build.sh
+++ user-thrift-service/deploy/build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+cd ../
+mvn package
+
+cd deploy
 docker build -t user-service:latest .
---
+++ user-thrift-service/deploy/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -it user-service:latest --mysql.address=192.168.1.16:3306
