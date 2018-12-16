cc8d7d14e8ba6ef7f44f07d96f589411f491b37a
lxcyha <253314157@qq.com>
2018-12-16 13:49:35 +0800

    using ip

--- course-edge-service/build.sh
+++ course-edge-service/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mvn package
-docker build -t course-edge-service:latest .
+docker build -t lxcyha/course-edge-service:latest .
+docker push lxcyha/course-edge-service:latest
--- course-edge-service/run.sh
+++ course-edge-service/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it course-edge-service:latest --zookeeper.address=192.168.1.16
+docker run -it lxcyha/course-edge-service:latest --zookeeper.address=192.168.1.16
